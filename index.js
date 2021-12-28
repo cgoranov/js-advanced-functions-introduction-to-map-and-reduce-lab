b6985467da80737aa5cdde68eb75040e1955d447
Che <cgoranov@gmail.com>
2021-12-27 18:28:06 -0600

    commit tree eedc7ef5912afe58265361caa566915543573ec0

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+
+function mapToNegativize(arr) {
+    const negArr = arr.map(function(element) {
+        return element * -1
+    })
+    return negArr
+}
+
+function mapToNoChange(arr) {
+    const sameArr = arr.map(element => element)
+    return sameArr
+}
+
+function mapToDouble(arr) {
+    const doubleArr = arr.map(element => element * 2)
+    return doubleArr
+}
+
+function mapToSquare(arr) {
+    const squareArr = arr.map(element => element ** 2)
+    return squareArr
+}
+
+function reduceToTotal (arr, start=0) {
+    const total = arr.reduce(function(accum, element) { return accum + element }, start)
+    return total
+}
+
+function reduceToAllTrue (arr) {
+    const result = arr.filter(element => !!element === false )
+    if (result.length === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function reduceToAnyTrue (arr) {
+    const result = arr.find(element => !!element === true )
+    if (!!result) {
+        return true
+    } else {
+        return false
+    }
+}
